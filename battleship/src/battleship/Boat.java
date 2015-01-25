df1547038823efa0e34f44912b445ef865323d3b
wal12060 <wal12060@byui.edu>
2015-01-24 21:23:25 -0600

    Got some thing together for the player stats as well as some thing for the boats...maybe between my code and Awnya's we can get the boat class working.

---
+++ battleship/src/battleship/Boat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author katiewalker
+ */
+public class Boat {
+    //Options for names
+    //Carrier (5)
+    //Battelship (4)
+    //Cruiser (3)
+    //Submarine (3)
+    //Destroyer (2)
+    String name;
+    
+    Boat(String shipName)
+    {
+        name == shipName;
+    }
+
+    
+            
+    
+}
---
+++ battleship/src/battleship/playerStatistics.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author katiewalker
+ */
+public class playerStatistics {
+    
+    int highScore; //displays high score and name of player
+    highScore == 0;
+    
+    int getScore()
+    {
+        
+        return highScore;
+    }
+        
+}
