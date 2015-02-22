f486688a7dc9d447258cc7d9f4a98176fe1a851d
Awnya Boam <boamhome@gmail.com>
2015-02-21 22:16:04 -0800

    update to Boat class to show hit and max damage along with messages

--- battleship/src/battleship/Boat.java
+++ battleship/src/battleship/Boat.java
@@ -28,4 +28,72 @@ public class Boat
          if(this.hitDamage== this.maxDamage)
                System.out.println("You sunk my " + this.name);
     }
+    
+       
+    
+    /*-------------------------------------------------------------------
+    Description:  Calculates Hits on a Ship, remaning hits and if the boat is sunk.
+                  Also calculate the % of the ship that remains unharmed. 
+    
+    Author:  Awnya Boam
+    Info:  Group Lesson #3 assignemnt
+    
+    input: HitDamage, maxDamage
+    Output: 0 for OK and -1 for Error
+    
+    --------------------------------------------------------------------*/
+    
+       
+    int hitOrSunk(int hitDamage, int maxDamage)
+    {
+        //this.hitDamage++;  --->  We will need to add this in at a later date
+        
+        int hitsRemaining;    //Requirement 1 - Two or more primitive Variables
+        double boatHitPercent, boatHitOutput;                 //Variables for typecasting
+        
+        if(maxDamage < 2 || maxDamage > 5)   //Requirement 3 - At least one Relational operator 
+        {
+            System.out.println("\nError: Invalid boat size.\n");  //min boat size = 2, max = 5.  Check to ensure maxHits is within those parameters.
+            return -1;
+        }
+        else if(hitDamage < 0 || (hitDamage > maxDamage))  //Check hitDamage is less than maxDamage and greater than 0
+        {
+            System.out.println("\nError: Invalid number of hits. \n");
+            return -1;
+        }
+                
+         else if (hitDamage == 0) // no hits yet statement
+        {
+            System.out.println("\nYour " + name + " is safe.  It has no hits yet. \n");
+            return 0; 
+        }
+        
+        else if (hitDamage == maxDamage) // ship sunk statement 
+        {
+            if (maxDamage == 4) //if the boat sunk is the battleship
+            {
+                System.out.println("\nYour " + name + " is sunk.  You LOSE.  Better luck next time!! \n");
+                return 0; 
+            }
+            
+            else //for any other ship
+            {
+                System.out.println("\nYour " + name + " is sunk.\n");
+                return 0; 
+            }          
+        }
+        
+        else
+        {
+            hitsRemaining = maxDamage - hitDamage;               //Requirement 2 - Two or More Mathmatical Operators
+           boatHitPercent = (double) hitDamage /maxDamage;
+                         
+            boatHitOutput = (boatHitPercent * 100);   //Requirement 4 - A type conversion using type casting       
+        
+        
+            System.out.println ("\nYour " + name + " got hit! \n\t You have " + hitsRemaining + " hits on your " + name + " remaining. \n\tYour " + name + " is damaged " + (int) boatHitOutput + "%. \n"); // Requirement 5 - at least two character escape sequences
+              return 0; //everything working correctly
+        }
+        
+    }
 }
