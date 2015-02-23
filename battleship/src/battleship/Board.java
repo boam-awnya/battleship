fb0ea5ce8a3779bbb7342a1b14883c691ad6b32d
Vehikite-John <jdavet1244@gmail.com>
2015-02-22 18:16:17 -0800

    Created getHitMiss() in Player class calculates number of hits and misses, then calls get geetGameStats() Created availableShots() in GameMenuControl class which outputs all of the available spaces to make a shot Added code to display an option for “Show available shot coordinates” (Option ‘A’) and code to call availableShots() in GameMenu class

--- battleship/src/battleship/Board.java
+++ battleship/src/battleship/Board.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -44,7 +45,7 @@ public class Board
         if(playerBoard)
             boardtype = "Player";
         else
-            boardtype = "Oponent";
+            boardtype = "Opponent";
                  
     }
 
@@ -178,5 +179,4 @@ public class Board
         }
         
     }
-
 }
