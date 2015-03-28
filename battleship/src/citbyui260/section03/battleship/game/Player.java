f3c6ed6f0f3b0113026f6162e8f51af3f3bb061c
r16bi_000 <r16bi_000@DavesLaptop>
2015-03-28 11:45:21 -0600

    Lesson 11 created player exceptions

--- battleship/src/citbyui260/section03/battleship/game/Player.java
+++ battleship/src/citbyui260/section03/battleship/game/Player.java
@@ -9,6 +9,7 @@ import citbyui260.section03.battleship.boards.ShotBoard;
 import citbyui260.section03.battleship.boards.ShipBoard;
 import citbyui260.section03.battleship.boards.Board;
 import citbyui260.section03.battleship.enums.*;  //Inport ENUM Class
+import citbyui260.section03.battleship.exceptions.PlayerException;
 
 /**
  *
@@ -173,20 +174,22 @@ public class Player
     --------------------------------------------------------------------*/
     
     
-     public int getGameStats(int hit, int miss)
+     public int getGameStats(int hit, int miss) throws PlayerException
     {
         double totalShots,hitPercent,missPercent;   //Requirement 1 - Two or more primitive Variables
         int hitOutput, missOutput;                  //Variables for typecasting
         
         if(hit == 0 && miss == 0)   //Requirement 3 - At least one Relational operator 
         {
-            System.out.println("\nError: You have taken no shots yet.  Try harder.");  //Check for 0 and print error
-            return -1;
+            throw new PlayerException("Error: You have taken no shots yet. Try harder.");
+//            System.out.println("\nError: You have taken no shots yet.  Try harder.");  //Check for 0 and print error
+//            return -1;
         }
         else if(hit < 0 || miss < 0)  //CHeck if Hit or miss are less than 0
         {
-            System.out.println("\nError: Invalid value in \"Hit\" or \"Miss\"\n");
-            return -1;
+            throw new PlayerException("Error: Invalid value in Hit or Miss");
+//            System.out.println("\nError: Invalid value in \"Hit\" or \"Miss\"\n");
+//            return -1;
         }
         else
         {
